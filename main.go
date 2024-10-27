2c8a388ce2fc7b5a5b51fbe562e730096465a84f
Yessentemir <yessentemir.utepov@gmail.com>
2024-10-28 01:40:31 +0500

    comments

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-// viewHandler генерирует ответ от браузура
+// viewHandler ответ на определенный запрос
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
 	message := []byte("Hello, web!")
 	_, err := writer.Write(message)
@@ -15,7 +15,7 @@ func viewHandler(writer http.ResponseWriter, request *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/hello", viewHandler)
-	err := http.ListenAndServe("localhost:8080", nil)
+	http.HandleFunc("/hello", viewHandler)            // регистрируем обработчик для сервера, сервер всегда ищет обработчики
+	err := http.ListenAndServe("localhost:8080", nil) // обрабатывает запрос браузера и отвечает на него, грубо говоря он ищет хендлеры
 	log.Fatal(err)
 }
