c7319a5464970796bb049eefc5a671db3e998c3b
Yessentemir <yessentemir.utepov@gmail.com>
2024-10-29 02:21:23 +0500

    new languages

--- main.go
+++ main.go
@@ -6,16 +6,27 @@ import (
 )
 
 // viewHandler ответ на определенный запрос
-func viewHandler(writer http.ResponseWriter, request *http.Request) {
-	message := []byte("Hello, web!")
-	_, err := writer.Write(message)
+func write(writer http.ResponseWriter, message string) {
+	_, err := writer.Write([]byte(message))
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+func englishHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "Hello, web!")
+}
+func frenchHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "Salut, web!")
+}
+func hindiHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "Namaste, web!")
+}
+
 func main() {
-	http.HandleFunc("/hello", viewHandler)            // регистрируем обработчик для сервера, сервер всегда ищет обработчики
+	http.HandleFunc("/hello", englishHandler)         // регистрируем обработчик для сервера, сервер всегда ищет обработчики
+	http.HandleFunc("/salut", frenchHandler)          // регистрируем обработчик для сервера, сервер всегда ищет обработчики
+	http.HandleFunc("/namaste", hindiHandler)         // регистрируем обработчик для сервера, сервер всегда ищет обработчики
 	err := http.ListenAndServe("localhost:8080", nil) // обрабатывает запрос браузера и отвечает на него, грубо говоря он ищет хендлеры
 	log.Fatal(err)
 }
